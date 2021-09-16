fa73d01ccb30ab5d86d41c3d777ece8a349a2ddf
LarisaLieb <larisamaximovna@gmail.com>
2021-09-16 19:53:38 +0200

    задание 3

---
+++ js/index3.js
@@ -0,0 +1,11 @@
+function show() {
+    switch (document.getElementById("screen").value)
+    {
+        case "yellow":
+            document.body.style.backgroundColor= "yellow"; break;
+            case "pink":
+                document.body.style.backgroundColor= "pink"; break;
+                case "blue":
+                    document.body.style.backgroundColor= "blue"; break; 
+    }
+}
