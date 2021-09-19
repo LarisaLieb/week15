b6d24a2aee302bc246268d49cff3936f6df062d8
LarisaLieb <larisamaximovna@gmail.com>
2021-09-19 15:48:38 +0200

    форма

--- js/index2.js
+++ js/index2.js
@@ -1,20 +1,28 @@
 
-$('#fancy-inputs input[type="text"]').blur(function(){
-  if($(this).val().length > 0){
-    $(this).addClass('white');
-  } else {
-    $(this).removeClass('white');
-  }
-});
+document.querySelector('#signup').addEventListener('click', validate);
 
-// Radios
-
-$("#reg-radio input[type=radio]").click(function() {
-  $('label.radio').removeClass('selected');
-  var inputID = $(this).attr('id');
-  if ($(this).is(':checked')) {
-    $('.' + inputID).addClass('selected');
-  } else {
-    $('.' + inputID).removeClass('selected');
-  }
-});
+function validate() {
+  let a = document.forms["Form"]["username"].value;
+if (a == "") {
+  alert("Укажите ваше имя");
+  return false;
+}
+let b = document.forms["Form"]["password"].value;
+if (b == "") {
+  alert("Укажите пароль");
+  return false;
+}
+let с = document.forms["Form"]["phone"].value;
+if (с == "") {
+  alert("Укажите ваш телефон");
+  return false;
+}
+let d = document.forms["Form"]["mail"].value;
+if (d == "") {
+  alert("Укажите ваш мэйл");
+  return false;
+}
+else {
+  alert (`Добро пожаловать ${a}`);
+}
+}
